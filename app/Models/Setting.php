9d39940ea260b563de13986bd04782db53a32b35
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2026-04-02 10:22:04 +0700

    setting seeder

---
+++ app/Models/Setting.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Cache;
+
+class Setting extends Model
+{    
+    /**
+     * fillable
+     *
+     * @var array
+     */
+    protected $fillable = ['name', 'logo_url'];
+
+    /**
+     * The "booted" method of the model.
+     */
+    protected static function booted(): void
+    {
+        static::saved(function (Setting $setting) {
+            Cache::forget('settiing');
+            Cache::rememberForever('setting', function () {
+                $setting = Setting::first();
+
+                if (!$setting) {
+                    $setting = Setting::create();
+                }
+
+                return [
+                    'logo_url' => $setting->logo_url,
+                    'name' => $setting->name
+                ];
+            });
+        });
+    }
+}
---
+++ database/seeders/SettingSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Setting;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Storage;
+
+class SettingSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $setting = Setting::first();
+
+        if (!$setting) {
+            $setting = new Setting;
+        }
+
+        Storage::put('logo.png', file_get_contents(storage_path('app/logo.png')));
+
+        $setting->logo_url = Storage::url('logo.png');
+        $setting->name = config('app.name');
+
+        $setting->save();
+    }
+}
