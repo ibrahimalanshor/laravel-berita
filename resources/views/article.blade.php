81102bb913b6661e9b195339063429f6b6af9e40
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2026-04-07 21:03:49 +0700

    article meta render

--- resources/views/article.blade.php
+++ resources/views/article.blade.php
@@ -2,14 +2,26 @@
 
 @section('content')
 
-<div class="py-4">
-    <x-base.container>
-        <nav class="pb-2 flex items-center gap-2 text-neutral-700 text-sm sm:border-b sm:border-neutral-300">
-            <a class="hover:underline" href="{{ route('home') }}">Beranda</a>
-            <span class="icon-[tabler--chevron-right]"></span>
-            <a class="hover:underline" href="{{ route('category.detail', ['slug' => $article->category->slug]) }}">{{ $article->category->name }}</a>
-        </nav>
-    </x-base.container>
-</div>
+<x-base.container class="py-4">
+    <nav class="pb-2 flex items-center gap-2 text-neutral-700 text-sm sm:border-b sm:border-neutral-300">
+        <a class="hover:underline" href="{{ route('home') }}">Beranda</a>
+        <span class="icon-[tabler--chevron-right]"></span>
+        <a class="hover:underline" href="{{ route('category.detail', ['slug' => $article->category->slug]) }}">{{ $article->category->name }}</a>
+    </nav>
+
+    <article>
+        <h1>{{ $article->title }}</h1>
+        <p>{{ $article->summary }}</p>
+
+        <div>
+            <a href="">Supriyanto</a>
+            <p>Terbit {{ formatDate($article->published_at) }}</p>
+
+            <button>
+                <span class="icon-[tabler--share-3]"></span>
+            </button>
+        </div>
+    </article>
+</x-base.container>
 
 @endsection
