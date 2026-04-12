28c0eb5025f0df74dfd0319b9ec4075e79d05999
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2026-04-12 11:43:20 +0700

    author page

--- resources/views/author.blade.php
+++ resources/views/author.blade.php
@@ -1,39 +1,36 @@
 @extends('layouts.home')
 
 @section('content')
-<x-base.container>
-    <article>
-        <img src="{{ $author->image_url }}" />
-        <h1>{{ $author->name }}</h1>
-        <div class="prose prose-neutral prose-a:text-sky-600 max-w-none mx-auto">{!! $author->about !!}</div>
-        <div>
-            <a target="_blank" href="" class="">
-                <span class="icon-[tabler--brand-whatsapp-filled]"></span>
-                <span class="">Whatsapp</span>
-            </a>
-            <a target="_blank" href="" class="">
-                <span class="icon-[tabler--brand-facebook-filled]"></span>
-                <span class="">Facebook</span>
-            </a>
-            <a target="_blank" href="" class="">
-                <span class="icon-[tabler--brand-twitter-filled]"></span>
-                <span class="">Twitter</span>
-            </a>
-            <a target="_blank" href="" class="">
-                <span class="icon-[tabler--brand-tiktok-filled]"></span>
-                <span class="">Tiktok</span>
-            </a>
-            <a target="_blank" href="" class="">
-                <span class="icon-[tabler--brand-youtube-filled]"></span>
-                <span class="">Youtube</span>
-            </a>
-            <a target="_blank" href="" class="">
-                <span class="icon-[tabler--mail-filled]"></span>
-                <span class="">Email</span>
-            </a>
+<article class="py-6 space-y-4 max-w-prose mx-auto px-4">
+    <div class="flex items-center gap-6">
+        <img src="{{ $author->image_url }}" alt="{{ $author->name }}" class="w-20 h-20 object-cover rounded-full shrink-0" />
+        <div class="space-y-2">
+            <h1 class="font-bold text-neutral-900 text-3xl">{{ $author->name }}</h1>
+            <div class="flex gap-2">
+                <a target="_blank" href="" class="">
+                    <span class="icon-[tabler--brand-whatsapp-filled] size-5 text-green-600"></span>
+                </a>
+                <a target="_blank" href="" class="">
+                    <span class="icon-[tabler--brand-facebook-filled] size-5 text-blue-600"></span>
+                </a>
+                <a target="_blank" href="" class="">
+                    <span class="icon-[tabler--brand-twitter-filled] size-5 text-sky-600"></span>
+                </a>
+                <a target="_blank" href="" class="">
+                    <span class="icon-[tabler--brand-tiktok-filled] size-5 text-neutral-900"></span>
+                </a>
+                <a target="_blank" href="" class="">
+                    <span class="icon-[tabler--brand-youtube-filled] size-5 text-red-600"></span>
+                </a>
+                <a target="_blank" href="" class="">
+                    <span class="icon-[tabler--mail-filled] size-5 text-neutral-600"></span>
+                </a>
+            </div>
         </div>
-    </article>
-</x-base.container>
+    </div>
+    <div class="prose prose-neutral prose-a:text-sky-600 max-w-none mx-auto">{!! $author->about !!}</div>
+    
+</article>
 
 <x-base.container :paddless="true" class="border-t border-neutral-200 pt-4 grid grid-cols-1 gap-6 sm:grid-cols-5 sm:px-4 lg:border-0 lg:pt-0">
     <section class="space-y-4 px-4 lg:border-b-0 sm:col-span-3 sm:px-0">
