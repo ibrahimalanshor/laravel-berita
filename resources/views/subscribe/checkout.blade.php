58d47f01d3bab5822f999b38dd4c647ef8e3e620
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2026-04-13 14:03:24 +0700

    checkout page

--- resources/views/subscribe/checkout.blade.php
+++ resources/views/subscribe/checkout.blade.php
@@ -2,20 +2,16 @@
 
 @section('content')
 <x-base.container>
-    <section>
-        <h1>Checkout Berlangganan Paket {{ $package->name }}</h1>
-        <p>Konfirmasi berlangganan paket {{ $package->name }} dengan melakukan pembayaran melalui metode yang tersedia.</p>
-
-        <div>
-            <h2>Total Harga: {{ number_format($package->price) }}</h2>
+    <section class="py-6 max-w-prose space-y-6 sm:py-8 lg:py-10">
+        <div class="space-y-2">
+            <h1 class="font-bold text-3xl text-neutral-900">Checkout Berlangganan Paket {{ $package->name }}</h1>
+            <p class="text-neutral-700">Konfirmasi berlangganan paket {{ $package->name }} dengan melakukan pembayaran melalui metode yang tersedia.</p>
         </div>
 
-        <form>
-            <h2>Email</h2>
-            <p>Masukkan email Anda untuk menerima notifikasi dan informasi terkait langganan Anda.</p>
-            <x-base.input type="email" name="email" placeholder="Email Anda" class="w-full" />
-            <x-base.button color="primary" class="w-full">
-                <p>Bayar Sekarang</p>
+        <form class="flex flex-col gap-4 items-center justify-between border p-4 border-neutral-200 rounded-lg sm:flex-row">
+            <p class="font-bold text-red-600 text-2xl">Total Harga: {{ number_format($package->price) }}</p>
+            <x-base.button color="primary" icon="{{ $package->price === 0 ? 'icon-[tabler--check]' : 'icon-[tabler--credit-card-filled]' }}" type="submit">
+                {{ $package->price === 0 ? 'Berlangganan Gratis' : 'Lanjutkan Pembayaran' }}
             </x-base.button>
         </form>
     </section>
