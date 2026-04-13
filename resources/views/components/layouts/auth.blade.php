c9a9c0f950ca0975b9d220c23e37620e1c2d4e65
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2026-04-13 21:14:40 +0700

    login page

---
+++ resources/views/components/layouts/auth.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <title>{{ $title ?? config('app.name') }}</title>
+
+        @if (isset($description))
+            <meta name="description" content="{{ $description }}">
+        @endif
+
+        <link rel="icon" type="image/png" sizes="16x16" href="{{ setting('icon_url') }}">
+
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+        @livewireStyles
+    </head>
+    <body>
+        @yield('content')
+
+        @livewireScripts
+
+        @stack('scripts')
+    </body>
+</html>
---
+++ resources/views/login.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.auth')
+
+@section('content')
+<main class="min-h-screen flex flex-col items-center justify-center bg-neutral-50 px-4">
+    <div class="max-w-sm flex flex-col items-center justify-center">
+        <x-includes.logo class="h-10" />
+        <div class="space-y-4 flex flex-col border w-full border-neutral-300 p-4 bg-white rounded-md mt-8 mb-4">
+            <div class="space-y-1">
+                <h1 class="font-bold text-xl text-neutral-900">Masuk atau Daftar</h1>
+                <p class="text-neutral-700">Gunakan layanan di bawah ini untuk masuk ke Lararita.</p>
+            </div>
+            <x-base.button color="primary" icon="icon-[tabler--brand-google-filled]">Lanjutkan dengan Google</x-base.button>
+        </div>
+        <a href="{{ route('home') }}" class="text-sm text-neutral-700">Kembali ke beranda</a>
+    </div>
+</main>
+@endsection
