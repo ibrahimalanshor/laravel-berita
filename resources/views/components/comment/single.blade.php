92df5486a8340247a0b538a9330d8b23d02c57bc
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2026-04-28 20:03:43 +0700

    comment report dropdown

--- resources/views/components/comment/single.blade.php
+++ resources/views/components/comment/single.blade.php
@@ -1,8 +1,32 @@
 <article class="flex items-start gap-3 relative">
     <div class="absolute top-0 right-0">
-        <button class="text-neutral-500 ml-auto">
+        <button class="text-neutral-500 ml-auto open-report-{{ $comment->id }} cursor-pointer" data-toggle="dropdown" data-target="#report-{{ $comment->id }}">
             <span class="icon-[tabler--dots] size-5"></span>
         </button>
+
+        <div id="report-{{ $comment->id }}" class="hidden shadow-md absolute top-6 right-0 bg-white border border-neutral-200 rounded-md p-4 space-y-2 z-10" data-click-outside-close="dropdown" data-ignore=".open-report-{{ $comment->id }}">
+            <p class="font-bold text-neutral-900">Laporkan Komentar</p>
+            <div class="flex flex-col gap-2">
+                <label class="whitespace-nowrap flex items-center gap-2 text-neutral-700">
+                    <input type="radio" name="report_type" value="spam" class="border-neutral-400 checked:border-red-700 focus:outline-red-700 checked:bg-red-700">
+                    Spam
+                </label>
+                <label class="whitespace-nowrap flex items-center gap-2 text-neutral-700">
+                    <input type="radio" name="report_type" value="spam" class="border-neutral-400 checked:border-red-700 focus:outline-red-700 checked:bg-red-700">
+                    Ujaran Kebencian
+                </label>
+                <label class="whitespace-nowrap flex items-center gap-2 text-neutral-700">
+                    <input type="radio" name="report_type" value="spam" class="border-neutral-400 checked:border-red-700 focus:outline-red-700 checked:bg-red-700">
+                    Kata-Kata Kasar
+                </label>
+                <label class="whitespace-nowrap flex items-center gap-2 text-neutral-700">
+                    <input type="radio" name="report_type" value="spam" class="border-neutral-400 checked:border-red-700 focus:outline-red-700 checked:bg-red-700">
+                    Hoaks / Misinformasi
+                </label>
+            </div>
+
+            <x-base.button color="primary" size="sm" icon="icon-[tabler--message-report-filled]" class="block w-full">Laporkan</x-base.button>
+        </div>
     </div>
     <img src="{{ $comment->avatar_url }}" alt="{{ $comment->name }}" class="size-8 rounded-full mt-1">
     <div class="grow space-y-2">
